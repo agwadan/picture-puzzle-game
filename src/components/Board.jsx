264a98eed257481bd8629a5299e451ce5b96d386
agwadan <agwa.dan@gmail.com>
2023-07-27 15:46:04 +0300

    Update animation

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Tile from './Tile';
 
+/* Board Containing the tiles 
+--- Takes in size of each tile and 
+--- the picture for the puzzle*/
+
 const Board = ({ size, pictureUrl }) => {
-  const [tileOrder, setTileOrder] = useState([...Array(16).keys()]);
+  const [tileOrder, setTileOrder] = useState([...Array(16).keys()]); //----------------- creating an array with 16 elements, each equal to its index. 
 
   useEffect(() => {
     let newTileOrder = [...tileOrder];
@@ -16,6 +20,22 @@ const Board = ({ size, pictureUrl }) => {
     setTileOrder(newTileOrder);
   }, []);
 
+  const handleClick = (id) => {
+    const position = tileOrder.indexOf(id);
+    const emptyPosition = tileOrder.indexOf(15);
+
+    if (
+      (position === emptyPosition - 1 && position % 4 !== 3) ||
+      position === emptyPosition + 1 ||
+      position === emptyPosition - 4 ||
+      position === emptyPosition + 4
+    ) {
+      const newTileOrder = [...tileOrder];
+      [newTileOrder[position], newTileOrder[emptyPosition]] = [newTileOrder[emptyPosition], newTileOrder[position]];
+      setTileOrder(newTileOrder);
+    }
+  };
+
   const tiles = [];
 
   for (let i = 0; i < tileOrder.length; i++) {
@@ -23,14 +43,16 @@ const Board = ({ size, pictureUrl }) => {
       <Tile
         key={i}
         id={tileOrder[i]}
+        position={i}
         size={size}
         pictureUrl={pictureUrl}
+        onClick={handleClick}
       />
     );
   }
 
   return (
-    <div className="board">
+    <div className="board" style={{ position: 'relative', width: size * 4, height: size * 4 }}>
       {tiles}
     </div>
   );
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
-const Tile = ({ size, id, pictureUrl }) => {
+const Tile = ({ size, id, pictureUrl,position, onClick }) => {
   return (
     <div
-      className="tile"
-      style={{
-        width: size,
-        height: size,
-        backgroundImage: `url(${pictureUrl})`,
-        backgroundSize: `${size * 4}px ${size * 4}px`,
-        backgroundPosition: `${-1 *(id % 4) * size}px ${-1 * Math.floor(id / 4) * size}px`,
-      }}
-    />
+    className={`tile ${id === 15 ? 'tile-empty' : ''}`}
+    onClick={() => onClick(id)}
+    style={{
+      width: size,
+      height: size,
+      backgroundImage: id === 15 ? null : `url(${pictureUrl})`,
+      backgroundSize: `${size * 4}px ${size * 4}px`,
+      backgroundPosition: `${-1 * (id % 4) * size}px ${-1 * Math.floor(id / 4) * size}px`,
+      transform: `translate(${(position % 4) * size}px, ${Math.floor(position / 4) * size}px)`,
+      position: 'absolute',
+      transition: 'transform 0.3s'
+    }}
+  />
   );
 };
 
